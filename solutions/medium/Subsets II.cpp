16c58829bdc9c8704791592dc4d7e0d3e30970b1
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-05 16:42:24 +0530

    Add solution for Subsets II

--- solutions/medium/Subsets II.cpp
+++ solutions/medium/Subsets II.cpp
@@ -3,4 +3,23 @@
             // Language: C++
             // Link: https://leetcode.com/problems/subsets-ii/
 
-c
+class Solution {
+private:
+    void find(int ind,int n,vector<int>& nums,vector<vector<int>>&ans,vector<int>&ds){
+        ans.push_back(ds);
+        for(int i=ind;i<n;i++){
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            find(i+1,n,nums,ans,ds);
+            ds.pop_back();
+        }
+
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>ds;
+        vector<vector<int>>ans;
+        int n=nums.size();
+        find(0,n,nums,ans,ds);
+        return ans;
