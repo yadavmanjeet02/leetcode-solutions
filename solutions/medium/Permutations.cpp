fd74ee34b0de3be9b7617070c27255b3315c2116
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-07 16:43:04 +0530

    Add solution for Permutations

---
+++ solutions/medium/Permutations.cpp
@@ -0,0 +1,26 @@
+// Title: Permutations
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/permutations/
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+    
+
+    }
+            solve(nums,ind+1,n);
+        }
+
+            
+            swap(nums[ind],nums[i]);
+            swap(nums[ind],nums[i]);
+        }
+        if(ind==n) {
+
+    void solve(vector<int>&nums,int ind,int n){
+  
+    vector<vector<int>>ans;
+            return;
+        for(int i=ind;i<n;i++){
+            ans.push_back(nums);
