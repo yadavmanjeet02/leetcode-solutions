005a0b7e057abc33bfa851c7671a231af93d4f3d
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-10 17:48:36 +0530

    Add solution for Implement Stack using Queues

---
+++ solutions/easy/Implement Stack using Queues.cpp
@@ -0,0 +1,21 @@
+// Title: Implement Stack using Queues
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/implement-stack-using-queues/
+
+    }
+    
+    bool empty() {
+        return q.empty();
+        
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
