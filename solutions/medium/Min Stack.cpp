16ab53f4c46bd8d59101bba56a32200ea0c061be
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-11 18:32:42 +0530

    Add solution for Min Stack

---
+++ solutions/medium/Min Stack.cpp
@@ -0,0 +1,26 @@
+// Title: Min Stack
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/min-stack/
+
+    void pop() {
+    
+        
+    }
+        }
+        
+                st.push(2*value-mini);
+                mini=value;
+            }
+                st.push(value);
+            }
+            else{
+        else{
+            if(value>mini){
+        }
+            mini=value;
+            st.push(value);
+        if(st.empty()){
+    void push(int value) {
+    
+    }
