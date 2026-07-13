06556fb592cc79b01d4c77200f17174b863ff866
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-13 11:46:15 +0530

    Add solution for Find the Index of the First Occurrence in a String

---
+++ solutions/easy/Find the Index of the First Occurrence in a String.cpp
@@ -0,0 +1,19 @@
+// Title: Find the Index of the First Occurrence in a String
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+
+        if(haystack.find(needle)!=string::npos){
+
+        }
+        else{
+            return -1;
+        }
+    }
+            return haystack.find(needle);
+};
