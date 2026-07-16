a519b81ab75f95755871709ebb270c9c0c1a0fa5
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-16 14:42:28 +0530

    Add solution for Binary Tree Zigzag Level Order Traversal

---
+++ solutions/medium/Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,26 @@
+// Title: Binary Tree Zigzag Level Order Traversal
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+                int index;
+
+                // Decide where to store the value.
+                TreeNode* node = q.front();
+                q.pop();
+
+
+            for(int i=0;i<size;i++){
+            vector<int> level(size);
+            // Allocate space beforehand.
+
+            int size = q.size();
+
+        while(!q.empty()){
+
+        bool leftToRight = true;
+
+        q.push(root);
+
+                if(leftToRight)
+                    index = i;
