60462f8daa0cbde0eed83fcdc6ec92c1ca87ffe5
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-18 14:54:32 +0530

    Add solution for Construct Binary Search Tree from Preorder Traversal

---
+++ solutions/medium/Construct Binary Search Tree from Preorder Traversal.cpp
@@ -0,0 +1,26 @@
+// Title: Construct Binary Search Tree from Preorder Traversal
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+            build(preorder,
+                  i,
+                  bound);
+
+        return root;
+    }
+
+        root->right =
+        // right subtree
+
+                  root->val);
+                  i,
+            build(preorder,
+        root->left =
+        // left subtree
+
+            new TreeNode(preorder[i++]);
+        TreeNode* root =
+
+            return NULL;
+        if(preorder[i] > bound)
