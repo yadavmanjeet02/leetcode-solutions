83242c73cd0ab9596401102e40f1feebccd98bb1
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-18 15:11:33 +0530

    Add solution for Validate Binary Search Tree

---
+++ solutions/medium/Validate Binary Search Tree.cpp
@@ -0,0 +1,25 @@
+// Title: Validate Binary Search Tree
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/validate-binary-search-tree/
+
+               check(
+                    )
+                &&
+                    lb
+                    root->left,
+                    root->val,
+        return check(
+
+            return false;
+           root->val<=lb)
+        if(root->val>=ub ||
+
+            return true;
+        if(root==NULL)
+
+               long long lb){
+
+    bool check(TreeNode* root,long long ub,
+private:
+class Solution {
