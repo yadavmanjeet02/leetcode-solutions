60639de6ab83209c51712872cc11e46e58c680d4
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-19 16:24:27 +0530

    Add solution for Binary Search Tree Iterator

---
+++ solutions/medium/Binary Search Tree Iterator.cpp
@@ -0,0 +1,26 @@
+// Title: Binary Search Tree Iterator
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/binary-search-tree-iterator/
+
+public:
+    BSTIterator(TreeNode* root) {
+        
+    }
+    
+    int next() {
+        pushAll(root);
+    }
+        }
+            root=root->left;
+        TreeNode* temp=st.top();
+        st.pop();
+            st.push(root);
+        while(root){
+    void pushAll(TreeNode* root){
+    stack<TreeNode*>st;
+class BSTIterator {
+private:
+ */
+ * };
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
