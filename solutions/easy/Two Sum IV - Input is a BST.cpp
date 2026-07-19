93187a0f700e00ed914a8df94a8d160c4eb7f805
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-19 16:08:24 +0530

    Add solution for Two Sum IV - Input is a BST

---
+++ solutions/easy/Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,6 @@
+// Title: Two Sum IV - Input is a BST
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/two-sum-iv---input-is-a-bst/
+
+/
