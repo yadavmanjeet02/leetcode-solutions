0ec3e7a948aab794a07287752655cbd285ecc2c4
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-20 15:51:33 +0530

    Add solution for Find Median from Data Stream

--- solutions/hard/Find Median from Data Stream.cpp
+++ solutions/hard/Find Median from Data Stream.cpp
@@ -3,24 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/find-median-from-data-stream/
 
-        
-
-        if(right.size()>left.size()){
-            left.push(right.top());
-        }
-            right.pop();
-    }
-    
-    double findMedian() {
-        
-    }
-
-        if(left.size()>right.size()){
-            return left.top();
-        }
-        return (left.top()+right.top())/2.0;
-};
-
-        left.pop();
-        right.push(left.top());
-
+c
