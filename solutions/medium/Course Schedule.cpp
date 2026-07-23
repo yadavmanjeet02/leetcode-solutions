62042a3c47de77c36b798282c586404b995cc359
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-07-23 21:43:14 +0530

    Add solution for Course Schedule

--- solutions/medium/Course Schedule.cpp
+++ solutions/medium/Course Schedule.cpp
@@ -3,4 +3,23 @@
             // Language: C++
             // Link: https://leetcode.com/problems/course-schedule/
 
-c
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(numCourses);
+
+        for(auto &p : prerequisites) {
+            adj[p[1]].push_back(p[0]);
+        }
+        int n =numCourses;
+        vector<int>vis(n,0);
+        vector<int>indegrees(n,0);
+
+        for(int i=0;i<n;i++){
+            for(auto it :adj[i]){
+                indegrees[it]++;
+            }
+
+        }
+        queue<int>q;
