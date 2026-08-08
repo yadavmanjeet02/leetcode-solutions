2fa3837b205280be5b9b3d8fc7b78929d6400012
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-09 02:24:25 +0530

    Add solution for Maximum Subarray

---
+++ solutions/medium/Maximum Subarray.cpp
@@ -0,0 +1,21 @@
+// Title: Maximum Subarray
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+        return maxi;
+            
+        }
+            
+
+            maxi=max(maxi,sum);
+        int maxi=0;
+    }
+        int neg=0;
+            sum+=nums[i];
+};
