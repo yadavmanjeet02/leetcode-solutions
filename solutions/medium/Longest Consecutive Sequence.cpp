15659a48228f4284dd59b875bf0f025b0ac4815c
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-09 02:34:41 +0530

    Add solution for Longest Consecutive Sequence

---
+++ solutions/medium/Longest Consecutive Sequence.cpp
@@ -0,0 +1,6 @@
+// Title: Longest Consecutive Sequence
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/longest-consecutive-sequence/
+
+c
