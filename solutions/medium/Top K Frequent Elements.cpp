67f555d0f6a2720da22cced650c74fb5d755fea0
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-10 03:23:27 +0530

    Add solution for Top K Frequent Elements

--- solutions/medium/Top K Frequent Elements.cpp
+++ solutions/medium/Top K Frequent Elements.cpp
@@ -3,24 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/top-k-frequent-elements/
 
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]+=1;
-        }
-        unordered_map<int,int>mpp;
-
-        for(auto it:mpp){
-        
-        
-            pq.push({it.second,it.first});
-        }
-            if(pq.size()>k){
-                pq.pop();
-            }
-        return ans;;
-        vector<int>ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top().second);
-        }
-    }
-            pq.pop();
-};
+c
