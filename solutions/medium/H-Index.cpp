3f93e6ca287312fa0e066cc77c6fe211e7a515b2
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-15 10:54:31 +0530

    Add solution for H-Index

---
+++ solutions/medium/H-Index.cpp
@@ -0,0 +1,20 @@
+// Title: H-Index
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/h-index/
+
+
+            
+
+        }
+        return h;
+    }
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+            if(cit[i]>=h) cnt++;
+        if(cnt<h) return check(cit,h-1);
+        for(int i=0;i<cit.size();i++){
+        int cnt=0;
+    int check(vector<int>&cit, int h){
+public:
+class Solution {
