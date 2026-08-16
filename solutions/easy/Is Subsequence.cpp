465f7539073d419c1164014c284197316802960c
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-17 01:30:41 +0530

    Add solution for Is Subsequence

---
+++ solutions/easy/Is Subsequence.cpp
@@ -0,0 +1,21 @@
+// Title: Is Subsequence
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/is-subsequence/
+
+        int m=t.size();
+        
+        if(n>m) return false;
+
+        while(i<n && j<m){
+            if(s[i]==t[j]){
+        }
+                i++;
+            }else{
+                j++;
+                j++;
+            }
+    }
+        return true;
+        if(i!=n) return false;
+};
