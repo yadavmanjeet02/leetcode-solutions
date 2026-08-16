2a1e7512666333975c3092bf470bb62cda28839a
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-17 01:35:56 +0530

    Add solution for Container With Most Water

--- solutions/medium/Container With Most Water.cpp
+++ solutions/medium/Container With Most Water.cpp
@@ -3,17 +3,17 @@
             // Language: C++
             // Link: https://leetcode.com/problems/container-with-most-water/
 
-            int area=h*(j-i);
-            maxarea=max(area,maxarea);
 
-            int h=min(height[i],height[j]);
-        while(i<j){
-
-        int maxarea=0;
-        int j=n-1;
-
-        int i=0;
-        int n=height.size();
-    int maxArea(vector<int>& height) {
-public:
-class Solution {
+            if(height[i]<height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+            
+        
+        }
+        return maxarea;
+        
+    }
+};
