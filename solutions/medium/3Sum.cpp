42ae6c1c850167012d079fe2c80d84c61a143a09
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-17 01:37:29 +0530

    Add solution for 3Sum

--- solutions/medium/3Sum.cpp
+++ solutions/medium/3Sum.cpp
@@ -3,4 +3,22 @@
             // Language: C++
             // Link: https://leetcode.com/problems/3sum/
 
- 
+
+                    });
+                        nums[k]
+                        nums[j],
+                        nums[i],
+                    ans.push_back({
+
+                if (sum == 0) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+
+            while (j < k) {
+
+            int k = n - 1;
+            int j = i + 1;
+
+                continue;
+            if (i > 0 && nums[i] == nums[i - 1])
+            // Skip duplicate first elements
