65cb09d820d9995b1a296ae66e0003e2cc806065
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-17 09:43:07 +0530

    Add solution for Longest Substring Without Repeating Characters

--- solutions/medium/Longest Substring Without Repeating Characters.cpp
+++ solutions/medium/Longest Substring Without Repeating Characters.cpp
@@ -3,4 +3,21 @@
             // Language: C++
             // Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
- 
+        while(r<s.size()){
+            if(hash[s[r]]!=-1){
+                if(hash[s[r]]>=l){
+                    l =hash[s[r]]+1;
+                }
+            }
+            int len =r-l+1;
+            maxlen =max(maxlen,len);
+            hash[s[r]] =r;
+            r++;
+        }
+        return maxlen;
+        l=r=maxlen=0;
+        int l,r,maxlen;
+        vector<int>hash(256,-1);
+    int lengthOfLongestSubstring(string s) {
+    }
+};
