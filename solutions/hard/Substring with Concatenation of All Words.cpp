3555adb0f0d81b0b21c7d7aa0c57034d718fb271
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-18 01:46:04 +0530

    Add solution for Substring with Concatenation of All Words

---
+++ solutions/hard/Substring with Concatenation of All Words.cpp
@@ -0,0 +1,23 @@
+// Title: Substring with Concatenation of All Words
+            // Difficulty: Hard
+            // Language: C++
+            // Link: https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+
+    vector<int> findSubstring(string s, vector<string>& words) {
+    }
+
+        }
+        return true;
+            } else {
+                return false;
+            }
+                }
+                if(--wordCount[w] == -1) {
+                    return false;
+            if(wordCount.find(w) != wordCount.end()) {
+            string w = s.substr(j, wordLen);
+    bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
+        for(int j=0; j<s.size(); j+=wordLen) {
+    
+public:
+class Solution {
