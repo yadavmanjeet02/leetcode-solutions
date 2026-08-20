37a027883a2d7f4cdb709e914148a1abf42a3332
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-20 18:09:21 +0530

    Add solution for Two Sum

--- solutions/easy/Two Sum.cpp
+++ solutions/easy/Two Sum.cpp
@@ -3,24 +3,18 @@
             // Language: C++
             // Link: https://leetcode.com/problems/two-sum/
 
-        while(i<j){
-
-            if(nums[i]+nums[j]<target){
-                i++;
+            else if(sum>target){
+                right--;
             }
+            else if(sum==target){
+                
+                return {temp[left].second,temp[right].second};
 
-        vector<int>ans;
-            else if(nums[i]+nums[j]>target){
-                j--;
-            }
-            else{
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-        sort(nums.begin(),nums.end());
             }
 
         }
-        return ans;
-        
+        return{};
+
     }
+};
+
