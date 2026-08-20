0af8a3e8cbd13bf4e0bc780ebfc65a712c0ee303
yadavmanjeet02 <manjeetyadavrj02@gmail.com>
2026-08-20 18:03:43 +0530

    Add solution for Word Pattern

---
+++ solutions/easy/Word Pattern.cpp
@@ -0,0 +1,24 @@
+// Title: Word Pattern
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/word-pattern/
+
+            char ch = pattern[i];
+
+                return false;
+
+            if (i == pattern.size())
+            // More words than pattern characters
+
+        while (ss >> word) {
+
+
+        int i = 0;
+        unordered_map<char, string> mp1;
+        unordered_map<string, char> mp2;
+        string word;
+
+
+        stringstream ss(s);
+    bool wordPattern(string pattern, string s) {
+public:
